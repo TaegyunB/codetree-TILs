ff8b12e3002e742e49c6bf00cb475858a80b5d3a
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-08-07 23:47:26 +0900

    Title: 그 요일은, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240807/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String[] name_day = new String[]{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        String day = sc.next();
+
+        int startDate = countDay(m1, d1);
+        int targetDate = countDay(m2, d2);
+
+        int remainDate = targetDate - startDate + 1;
+        
+        int targetDateIdx = 0;
+        for(int i=0; i<name_day.length; i++){
+            if(name_day[i].equals(day)){
+                targetDateIdx = i;
+            }
+        }
+
+        int count = 0;
+        int startDateIdx = 1;
+        for(int i=0; i<remainDate; i++){
+            int currentDateidx = (startDateIdx + i) % 7;
+            if(targetDateIdx == currentDateidx){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    public static int countDay(int m, int d){
+        int[] months = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int days = 1;
+
+        for(int i=1; i<m; i++){
+            days += months[i];
+        }
+
+        days += d;
+        return days;
+    }
+}

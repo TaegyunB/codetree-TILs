efae72743f420e3d4ccd2b786f0e8fbfd2ac0858
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-08-07 19:46:37 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240807/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        
+        String[] days = new String[]{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        int startDay = countDays(m1, d1);
+        int endDay = countDays(m2, d2);
+
+        int diff = endDay - startDay + 1; // 양쪽 끝의 날짜를 포함하기 위해서 +1
+        // -> ex) 두 날짜가 3월 1일과 3월 3일이라면, 1일부터 3일까지 총 3일이 포함되어야 함
+
+        System.out.println(days[diff % 7]);
+    }
+
+        
+    public static int countDays(int m, int d){
+        int[] num_of_month = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int days = 0; // 누적 일수 저장
+
+        for(int i=1; i<m; i++){ // 이전 달의 일수를 모두 더함
+            days += num_of_month[i];
+        }
+
+        days += d; // 해당 달의 일수를 더함
+        return days;
+    }
+}

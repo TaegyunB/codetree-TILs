b6547f9f27b623b91f602abc22bea34277c8450b
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-08-19 18:09:13 +0900

    Title: 최대로 겹치는 구간, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240819/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 240819/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -6,13 +6,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] arr = new int[100];
+        int[] arr = new int[200];
 
         for(int i=0; i<n; i++){
             int x = sc.nextInt();
             int y = sc.nextInt();
 
-            for(int j=x+1; j<=y; j++){
+            x += 100;
+            y += 100;
+
+            for(int j=x; j<y; j++){
                 arr[j] += 1;
             }
         }

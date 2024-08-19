6b33aa2698e89047685c785cffa07c8ee111cae7
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-08-19 23:35:31 +0900

    Title: 왔다 갔던 구역 2, Time: 143ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240819/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int start = 0;
+        int[] arr = new int[201];
+        int offset = 100;
+
+        for(int i=0; i<n; i++){
+            int x = sc.nextInt();
+            char way = sc.next().charAt(0);
+
+            if(way == 'R'){
+                for(int j=start; j<=start+x; j++){
+                    arr[j+offset]++;
+                }  
+                start += x;
+                
+            }
+            else if(way == 'L'){
+                if(start < 0){
+                    start += x;
+                }
+                for(int j=start; j>start-x; j--){
+                    arr[j+offset]++;
+                }
+                start -= x;
+            }
+        }
+
+        int cnt = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] >= 2){
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}

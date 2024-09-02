cb16bf22ee297e095bd065bf250df8b72a3cd48a
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-09-02 17:49:01 +0900

    Title: 계속 중첩되는 사각형, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240902/계속 중첩되는 사각형/continuously-overlapping-squares.java
+++ 240902/계속 중첩되는 사각형/continuously-overlapping-squares.java
@@ -25,7 +25,7 @@ public class Main {
         int cnt = 0;
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr[i].length; j++){
-                if(arr[i][j] == 2){
+                if(arr[i][j] == n){
                     cnt++;
                 }
             }

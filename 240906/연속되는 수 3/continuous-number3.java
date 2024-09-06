2192f755ee8287f1fd101a849fd7338020cb59ae
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-09-06 09:53:02 +0900

    Title: 연속되는 수 3, Time: 209ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240906/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<n; i++){
+            int num = sc.nextInt();
+            list.add(num);
+        }
+
+        int cnt = 0;
+        int maxCnt = 0;
+        for(int i=0; i<n; i++){
+            if(i == 0 || list.get(i) * list.get(i-1) > 0){
+                cnt++;
+            }
+            else{
+                maxCnt = Math.max(cnt, maxCnt);
+                cnt = 1;
+            }
+        }
+        maxCnt = Math.max(cnt, maxCnt);
+
+        System.out.println(maxCnt);
+        
+    }
+}

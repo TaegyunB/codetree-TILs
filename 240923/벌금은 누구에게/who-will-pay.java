2a5d148d23b02c6bef8cfb898e00f6354e3edbb2
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-09-23 01:05:00 +0900

    Title: 벌금은 누구에게, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240923/벌금은 누구에게/who-will-pay.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        int[] penaltyCounts = new int[n+1];
+        int count = 1;
+        boolean penaltyMoney = false;
+
+        for(int i=0; i<m; i++){
+            int student = sc.nextInt();
+            penaltyCounts[student] += count;
+
+            if(penaltyCounts[student] == k){
+                System.out.println(student);
+                penaltyMoney = true;
+                break;
+            }
+        }
+
+        if(penaltyMoney == false){
+            System.out.println(-1)
+        }
+    }
+}

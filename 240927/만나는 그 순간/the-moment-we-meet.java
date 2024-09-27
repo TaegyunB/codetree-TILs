941446240107d45eac8aadb7e1cac86f9fe62687
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-09-27 09:20:04 +0900

    Title: 만나는 그 순간, Time: 427ms, Memory: 37MB, Status: Passed - CodeTree

--- 240927/만나는 그 순간/the-moment-we-meet.java
+++ 240927/만나는 그 순간/the-moment-we-meet.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,68 +6,44 @@ public class Main {
 
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int[] a = new int[1000];  // 배열 크기 고정
-        int[] b = new int[1000];  // 배열 크기 고정
-        int Aloc = 0;  // 배열 인덱스를 0으로 시작
+        
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+        
+        a.add(0); // Starting position
+        b.add(0); // Starting position
+        
         int Acnt = 0;
-        int Bloc = 0;  // 배열 인덱스를 0으로 시작
         int Bcnt = 0;
 
-        // A의 이동
-        for(int i = 0; i < n; i++){
-            char d = sc.next().charAt(0);
+        for(int i=0; i<n; i++){
+            String d = sc.next();
             int t = sc.nextInt();
 
-            if(d == 'R'){
-                for(int j = 0; j < t; j++){
-                    Acnt++;
-                    if (Aloc < 1000) {  // 배열 인덱스가 1000을 초과하지 않도록 방지
-                        a[Aloc] = Acnt;
-                        Aloc++;
-                    }
-                }
-            } else if(d == 'L'){
-                for(int j = 0; j < t; j++){
-                    Acnt--;
-                    if (Aloc < 1000) {
-                        a[Aloc] = Acnt;
-                        Aloc++;
-                    }
-                }
+            int direction = d.equals("R") ? 1 : -1;
+            for(int j=0; j<t; j++){
+                Acnt += direction;
+                a.add(Acnt);
             }
         }
 
-        // B의 이동
-        for(int i = 0; i < m; i++){
-            char d = sc.next().charAt(0);
+        for(int i=0; i<m; i++){
+            String d = sc.next();
             int t = sc.nextInt();
 
-            if(d == 'R'){
-                for(int j = 0; j < t; j++){
-                    Bcnt++;
-                    if (Bloc < 1000) {
-                        b[Bloc] = Bcnt;
-                        Bloc++;
-                    }
-                }
-            } else if(d == 'L'){
-                for(int j = 0; j < t; j++){
-                    Bcnt--;
-                    if (Bloc < 1000) {
-                        b[Bloc] = Bcnt;
-                        Bloc++;
-                    }
-                }
+            int direction = d.equals("R") ? 1 : -1;
+            for(int j=0; j<t; j++){
+                Bcnt += direction;
+                b.add(Bcnt);
             }
         }
 
+        int minSize = Math.min(a.size(), b.size());
         boolean found = false;
-        int minLoc = Math.min(Aloc, Bloc);  // 두 사람의 최소 이동 위치까지만 비교
 
-        // 값 비교
-        for(int i = 0; i < minLoc; i++){  // 인덱스는 0부터 시작
-            if(a[i] == b[i]){
-                System.out.println(i + 1);  // 인덱스가 0부터 시작했으므로 출력은 +1
+        for(int i=1; i<minSize; i++){
+            if(a.get(i).equals(b.get(i))){
+                System.out.println(i);
                 found = true;
                 break;
             }
@@ -75,6 +51,6 @@ public class Main {
 
         if(!found){
             System.out.println(-1);
-        }
+        }    
     }
 }

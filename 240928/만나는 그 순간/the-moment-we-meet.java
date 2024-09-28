1bd0950548b2f73655534c7b0bfcedc47ef16482
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-09-28 17:53:39 +0900

    Title: 만나는 그 순간, Time: 290ms, Memory: 91MB, Status: Passed - CodeTree

---
+++ 240928/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] a = new int[10000000];
+        int[] b = new int[10000000];
+        int Aloc = 1;
+        int Acnt = 0;
+        int Bloc = 1;
+        int Bcnt = 0;
+
+
+        for(int i=0; i<n; i++){
+            char d = sc.next().charAt(0);
+            int t = sc.nextInt();
+
+            if(d == 'R'){
+                for(int j=0; j<t; j++){
+                    Acnt++;
+                    a[Aloc] = Acnt;
+                    Aloc++;
+                }
+            }
+            else if(d == 'L'){
+                for(int j=0; j<t; j++){
+                    Acnt--;
+                    a[Aloc] = Acnt;
+                    Aloc++;
+                }
+            }
+        }
+
+        for(int i=0; i<m; i++){
+            char d = sc.next().charAt(0);
+            int t = sc.nextInt();
+
+            if(d == 'R'){
+                for(int j=0; j<t; j++){
+                    Bcnt++;
+                    b[Bloc] = Bcnt;
+                    Bloc++;
+                }
+            }
+            else if(d == 'L'){
+                for(int j=0; j<t; j++){
+                    Bcnt--;
+                    b[Bloc] = Bcnt;
+                    Bloc++;
+                }
+            }
+        }
+
+        boolean found = false;
+        int minLoc = Math.min(Aloc, Bloc);
+
+        // for(int i=1; i<minLoc; i++){
+        //     System.out.printf("%d: %d %d\n", i, a[i], b[i]);
+        // }
+
+        for(int i=1; i<minLoc; i++){
+            if(a[i] == b[i]){
+                System.out.println(i);
+                found = true;
+                break;
+            }
+        }
+
+        if(found == false){
+            System.out.println(-1);
+        }    
+    }
+}

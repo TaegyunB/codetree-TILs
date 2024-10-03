bdddc9f4d43e7064e4c7ca82f4dad1948e19322d
TaegyunB <147308244+TaegyunB@users.noreply.github.com>
2024-10-03 20:35:34 +0900

    Title: 방향에 맞춰 이동, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241003/방향에 맞춰 이동/move-in-direction.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] dx = new int[]{1, 0, -1, 0};
+        int[] dy = new int[]{0, -1, 0, 1};
+        int nx = 0;
+        int ny = 0;
+
+        for(int i=0; i<n; i++){
+            char dir = sc.next().charAt(0);
+            int amount = sc.nextInt();
+
+            if(dir == 'E'){
+                nx += amount + dx[0] - 1;
+            }
+            else if(dir == 'S'){
+                ny += amount + dy[1] - 1;
+            }
+            else if(dir == 'W'){
+                nx += amount + dx[2] - 1;
+            }
+            else{
+                ny += amount + dy[3] - 1;
+            }
+        }
+        System.out.println(nx + " " + ny);
+    }
+}
